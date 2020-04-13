fa611c4aa9eb7db4241906a3ef05f45f5b4dd188
hgfkeep <hgfkeep@gmail.com>
2020-04-13 08:46:17 +0800

    UPDATE

--- serde_custom_net_io/src/main.rs
+++ serde_custom_net_io/src/main.rs
@@ -1,3 +1,17 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+use log::{debug, info};
+
+#[derive(Debug, PartialEq)]
+struct KubeConfig{
+    port: u8,
+    healthz_port: u8,
+    max_pos: u8
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+
 }
