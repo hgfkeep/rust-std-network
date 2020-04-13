57f0b4e51231f6aa05ab41a07ad0659dca9fe559
hgfkeep <hgfkeep@gmail.com>
2020-04-13 08:46:38 +0800

    FORMAT

--- serde_custom_net_io/src/main.rs
+++ serde_custom_net_io/src/main.rs
@@ -4,14 +4,12 @@ extern crate env_logger;
 use log::{debug, info};
 
 #[derive(Debug, PartialEq)]
-struct KubeConfig{
+struct KubeConfig {
     port: u8,
     healthz_port: u8,
-    max_pos: u8
+    max_pos: u8,
 }
 
 fn main() {
     env_logger::init();
-
-
 }
